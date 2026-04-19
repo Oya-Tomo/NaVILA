ee744b5085ebcac183c4bba5e6b83c7722dd3d39
Oya-Tomo <82521012+Oya-Tomo@users.noreply.github.com>
2026-04-19 16:38:37 +0900

    update: fixed ui

--- examples/manual_model_test.py
+++ examples/manual_model_test.py
@@ -31,7 +31,7 @@ parser.add_argument("--do_sample", action="store_true", default=False, help="Ena
 parser.add_argument("--temperature", type=float, default=0.7, help="Sampling temperature, only used when --do_sample is set (default: 0.7)")
 parser.add_argument("--model_path", type=str, default="a8cheng/navila-llama3-8b-8f", help="HuggingFace model path")
 parser.add_argument("--port", type=int, default=5000, help="Port to run the server on (default: 5000)")
-parser.add_argument("--demo_dir", type=str, default="demo", help="Directory containing index.html (default: examples/demo)")
+parser.add_argument("--demo_dir", type=str, default="demo", help="Directory containing index.html (default: demo)")
 args = parser.parse_args()
 
 # ---------------------------------------------------------------------------
